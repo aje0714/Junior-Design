ff9137c9311cc0f77929e71b4c32aa344c295e5b
IsabellaTerick <102822026+IsabellaTerick@users.noreply.github.com>
2024-07-01 22:22:14 -0400

    Update assignment8_one_LED.ino

--- assignment8_one_LED.ino
+++ assignment8_one_LED.ino
@@ -1,24 +1,103 @@
-#define LED1 13
-#define BUTTON 7
+#include <LiquidCrystal_I2C.h>
 
-//this is amber's comment :)
-//arissa's comment <3
+//Define pins here - needs redefining
+#define StartBtn 23
+#define RedRGB 5
+#define BlueRGB 6
+#define GreenRGB 7
+#define CoinBtn 9
+#define UpBtn 10
+#define DownBtn 11
+#define LeftBtn 12
+#define RightBtn 13
+
+LiquidCrystal_I2C lcd(0x3F, 16, 2);
+
+//Score variable
+int score = 0;
+
+//Time speed variable
+int speed = 1;
+
+//Pinmode Setup
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(LED1, OUTPUT);
-  pinMode(BUTTON, INPUT);
+  lcd.begin(16, 2);
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(5, 0);
+  lcd.print("0");
+
+  pinMode(StartBtn, INPUT);
+
 }
 
+//Code that is continuously ran
 void loop() {
-  // put your main code here, to run repeatedly:
-  if(digitalRead(BUTTON) == HIGH){
-    digitalWrite(LED1, HIGH);
-    delay(1000);
-    digitalWrite(LED1, LOW);
-    delay(1000);
+  //If start button pushed
+  if(digitalRead(StartBtn) == HIGH) {
+    runRound();
   }
-  else{
-    digitalWrite(LED1, LOW);
+}
+
+//Method used to run rounds of the game
+void runRound() {
+  //Variable used to determine if game should continue running
+  bool runGame = true; 
+
+  while(runGame) {
+    //randomly pick a number between [1,3]
+    int choice = random(1,3);
+
+    //switch for choices
+    switch(choice) {
+      //Move joystick
+      case 1:
+        makeNoise(1);
+        //timer
+        //if moved
+        if
+        
+        break;
+      //Press coin slot button
+      case 2:
+        makeNoise(2);
+        //timer
+
+        
+        break;
+      //Squeeze stress ball
+      case 3:
+        makeNoise(3);
+        //timer
+
+
+        break;
+      default:
+        break;
+    }
   }
+}
+
+//Method used to sound audio from MP3
+void makeNoise(int choice) {
 
 }
+
+//Method used to display score to LCD screen
+void displayScore() {
+
+}
+
+/*
+  Need special case for when you reach level 99
+*/
+//Method used when game is over
+void gameOver() {
+  //Play game over noise
+  makeNoise(4);
+  //Turn on Red LED
+  digitalWrite(RedLED, HIGH);
+  //Display final score on LCD screen
+  displayScore();
+}
+
